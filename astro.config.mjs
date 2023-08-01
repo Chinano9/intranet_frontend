a42611e80ce74383dc6710966f6588c19ff2eb38
Fernando Ponce Solis <ponceconc@outlook.com>
2023-08-01 07:39:48 -0600

    Agregadas dependencias como auth.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
+import auth from 'auth-astro';
+import { loadEnv } from 'vite';
+import Google from '@auth/core/providers/google';
 const DEV_PORT = 2121;
+
+ const env = loadEnv('production', process.cwd(), '');
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,19 +15,24 @@ export default defineConfig({
 		: `http://localhost:${DEV_PORT}`,
 	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
 
-	// output: 'server',
-
 	/* Like Vercel, Netlify,… Mimicking for dev. server */
 	// trailingSlash: 'always',
-
+  output: 'server',
 	server: {
 		/* Dev. server only */
 		port: DEV_PORT,
 	},
-
 	integrations: [
 		//
 		sitemap(),
 		tailwind(),
+		// auth({
+		// 	providers: [
+		// 		Google({
+		// 			clientId: env.GOOGLE_ID,
+		// 			clientSecret: env.GOOGLE_SECRET,
+		// 		}),
+		// 	],
+		// }),
 	],
 });
