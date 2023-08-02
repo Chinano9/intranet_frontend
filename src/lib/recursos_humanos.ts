623044fb7a59bef231fcb92055308a8d4b9607b3
Fernando Ponce Solis <ponceconc@outlook.com>
2023-08-02 07:14:22 -0600

    cambios en las conexiones a la API

--- src/lib/recursos_humanos.ts
+++ src/lib/recursos_humanos.ts
@@ -1,82 +1,96 @@
-import { API_URL } from "../app/constants";
-import type { Empleado } from "../types/recursos_humanos";
+import { API_URL } from '../app/constants';
+import type { Empleado } from '../types/recursos_humanos';
 const ENDPOINT = 'recursos_humanos/';
 
 export const url = `${API_URL}${ENDPOINT}`;
 
-const getEmpleados = async(query?:string, page?:string) => {
+const getEmpleados = async (query?: string, page?: string) => {
 	console.log('getEmpleados');
 
 	let endpoint = `${url}empleados/?`;
-	if (query) endpoint += `query=${query}&`; 
-	if (page) endpoint += `page=${page}`; 
+	if (query) endpoint += `query=${query}&`;
+	if (page) endpoint += `page=${page}`;
 
-	const request:RequestInit = {
-		method: 'GET'
+	const request: RequestInit = {
+		method: 'GET',
 	};
 	const data = await getData(endpoint, request);
 	return data;
-}
+};
 
-const postEmpleado = async(body:any) => {
+const postEmpleado = async (body: any) => {
 	console.log('getEmpleados');
 
 	const endpoint = `${url}empleados/`;
 
-	const request:RequestInit = {
+	const request: RequestInit = {
 		method: 'POST',
-		body:JSON.stringify(body)
+		headers: {
+			'Content-Type': 'application/json; charset=UTF-8',
+		},
+		body: JSON.stringify(body),
 	};
 
 	const data = await getData(endpoint, request);
 	return data;
-}
+};
 
-const deleteEmpleado = async(num_empleado:number,body:Empleado) => {
+const deleteEmpleado = async (num_empleado: number, body: Empleado) => {
 	console.log('getEmpleados');
 
 	const endpoint = `${url}empleados/${num_empleado}`;
 
-	const request:RequestInit = {
+	const request: RequestInit = {
 		method: 'PATCH',
-		body:JSON.stringify(body)
+		body: JSON.stringify(body),
 	};
 
 	const data = await getData(endpoint, request);
 	return data;
-}
+};
 
-const patchEmpleado = async(num_empleado:number,body:Empleado) => {
+const patchEmpleado = async (num_empleado: number, body: Empleado) => {
 	console.log('getEmpleados');
 
 	const endpoint = `${url}empleados/${num_empleado}`;
 
-	const request:RequestInit = {
+	const request: RequestInit = {
 		method: 'PATCH',
-		body:JSON.stringify(body)
+		body: JSON.stringify(body),
 	};
 
 	const data = await getData(endpoint, request);
 	return data;
+};
+
+const getEmpleado =async (num_empleado:number) => {
+	console.log('getEmpleados');
+
+	let endpoint = `${url}empleados/${num_empleado}`;
+
+	const request: RequestInit = {
+		method: 'GET',
+	};
+	const data = await getData(endpoint, request);
+	return data;
 }
 
-
-const getData = async (endpoint:string, request:RequestInit) =>{
+const getData = async (endpoint: string, request: RequestInit) => {
 	try {
-    const response = await fetch(endpoint, request);
-    const data =  await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error al obtener la lista de empleados:', error);
-    return [];
-  }
-}
-
+		const response = await fetch(endpoint, request);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error al obtener la lista de empleados:', error);
+		return [];
+	}
+};
 
 export {
 	getEmpleados,
 	postEmpleado,
 	patchEmpleado,
 	deleteEmpleado,
-	getEmpleado
-}
+	getEmpleado,
+	getData,
+};
