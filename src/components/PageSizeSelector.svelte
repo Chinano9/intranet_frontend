34a97df693ab067664fb082e05039e03359becdb
Fernando Ponce Solis <ponceconc@outlook.com>
2023-09-13 15:06:39 -0600

    despues de la actualizacion, todo parece ir bien

--- src/components/PageSizeSelector.svelte
+++ src/components/PageSizeSelector.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 	let urlParams = new URLSearchParams(window.location.search);
 
-	function handleClick(event) {
-		event.
-		urlParams.set('page_size', page);
+	function handleSelect(event: Event) {
+		const pageSize: string = event.value;
+		urlParams.set('page_size', pageSize);
 		window.location.replace(
 			`..${window.location.pathname}?${urlParams.toString()}`,
 		);
 	}
 </script>
 
-<select on:input={handleClick} form="search" id="">
+<select on:input={handleSelect} form="search">
 	<option value="10">10</option>
 	<option value="20">20</option>
 	<option value="50">50</option>
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,22 +1,29 @@
-import { defineMiddleware } from "astro:middleware";
-
+import { sequence } from '';
 // `context` and `next` are automatically typed
-export const onRequest = defineMiddleware((context, next) => {
-
-});
-
-
 const passthroughRoutes = ['/sign-in', '/'];
 
 function skipMiddleware(urlPathname: string) {
-  let shouldSkip = false;
-
+	let shouldSkip = false;
+	
+  // eslint-disable-next-line no-restricted-syntax
   for (const route of passthroughRoutes) {
-    if (urlPathname.startsWith(route)) {
-      shouldSkip = true;
+		if (urlPathname.startsWith(route)) {
+			shouldSkip = true;
       break;
     }
   }
-
+	
   return shouldSkip;
 }
+
+async function isAuthenticated({locals}, next:Function) {
+	locals.user.isAuthenticated
+	return next();
+}
+
+async function hasPermisions({locals}, next) {
+	
+}
+
+
+export const onRequest = sequence(isAuthenticated, hasPermisions);
